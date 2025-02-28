0eaa7210e7bc16f9b1c985f86dd2df042ab1f596
ENNACHAT Redwan <100237071+EnnachatRedwan@users.noreply.github.com>
2025-02-28 21:01:04 +0000

    initial commit

---
+++ src/main/java/com/gamacore/optaplaner/OptaplanerApplication.java
@@ -0,0 +1,13 @@
+package com.gamacore.optaplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptaplanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptaplanerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gamacore/optaplaner/domain/Room.java
@@ -0,0 +1,23 @@
+package com.gamacore.optaplaner.domain;
+
+public class Room {
+
+    private String name;
+
+    public Room() {
+    }
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
